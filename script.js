1911fc3b33b562055a8a6af132e7c903d62bd130
Aaron Regalado <regalado2829@gmail.com>
2022-04-06 16:43:43 -0400

    Almost Finished With Script
    The script for the Tic Tac Toe game is almost finished apart from a few
    components. The player can no longer click in a box with an 'O' in it.
    Also, the computer's choice has been refined. Finally, I started writing
    code in order to check for a game win.

--- script.js
+++ script.js
@@ -4,13 +4,47 @@ let filledBox =
 [false, false, false,
 false, false, false,
 false, false, false];
+let excludedValues = [];
+let computerIndexes = [];
+let playerIndexes = [];
+let gameWon = false;
+const possibilities = [
+    /* Rows */
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    /* Columns */
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    /* Diagonals */
+    [0,4,8],
+    [2,4,6]
+]
 
-for (let i=0; i<9; i++) {
+for (let i=0; i<=9; i++) {
     boardCells[i].addEventListener('click', function(){
+        if (computerIndexes == )
         if (filledBox[i] == false) {
             filledBox[i] = true;
             console.log(filledBox[i]);
             boardCells[i].textContent="X";
+            playerIndexes.push(i);
+            excludedValues.push(i);
+            if (excludedValues.length < 8) {
+                let computerChoice = Math.floor(Math.random() * filledBox.length);
+                while (excludedValues.includes(computerChoice)) {
+                    computerChoice = Math.floor(Math.random() * filledBox.length); 
+                }
+                excludedValues.push(computerChoice);
+                filledBox[computerChoice] = true;
+                boardCells[computerChoice].textContent="O";
+                computerIndexes.push(computerChoice);
+                console.log(computerChoice);
+                console.log(excludedValues);
+                console.log(playerIndexes);
+                console.log(computerIndexes);
+            }
         }
     })
 }
